e3113d0be80304eae3c70ba8f81df5eedfdcd2e6
Eduardo WB <eduardo@agenciacrow.com.py>
2021-03-26 10:51:06 -0300

    install laravel breeze for login

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/404-tenant', 'errors.404-tenant')->name('404.tenant');
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
